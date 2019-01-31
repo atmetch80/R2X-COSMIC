99fc6c0e4933c42ea9f9a203d0f1a2bc536293d4
atmetch80 <47217820+atmetch80@users.noreply.github.com>
2019-01-31 14:29:16 -0500

    Initial commit

---
+++ gg.py
@@ -0,0 +1,31 @@
+print(" what are you doing in this here town boy? ")
+pancakes = input()
+if pancakes == "Looking for a place to stay the night":
+    print("There is a motel down the lane")
+else:
+    print("Well then leave now or were going to have some trouble now boy")
+tacos = input()
+if tacos == "ill get out of your hair now thankyou for all your help":
+    print("Alright good day")
+else:
+    print("what did you say to me boy")
+icecream = input()
+if icecream == "one more question?":
+    print ("yes?")
+else:
+    print ("you better leave now or ima have to apply force boy")
+funnelcake = input()
+if funnelcake == "where is the church":
+    print ("down by harpers lane, i got to get going now good day")
+else:
+    print ("he punches you, then takes you down to the ground and ties your hands with a small rope.")
+    input()
+    print("He takes you down to the county jail and locks you in a dirty cell")
+    print("later that night he beats on the bars with a stick and saisy meal time jailbird")
+    print("he is holding a bowl of chlop as he tries to set it at the floor of your cell you grab his arm you pull him towards the cell with brutal force and knock him out, you grab the keys to your cell and mosey along down the dark hallway to the door you walk out with ease and head down to the chhurch")
+print("You walk down harpers lane where you see the church you see the open door you walk in and sit and start to pray the pastor walks up to you and says why are you here boy")
+sausage = input()
+if sausage == "looking for guidence":
+    print("there is a saloon down the road")
+else:
+    print("there is a saloon down the lane")
