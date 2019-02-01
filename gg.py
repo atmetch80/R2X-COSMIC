5e45b16b88226e7ea1f7062bb518f43bff06a9b5
atmetch80 <47217820+atmetch80@users.noreply.github.com>
2019-02-01 14:23:38 -0500

    points

--- gg.py
+++ gg.py
@@ -1,23 +1,27 @@
-
+score = 0
 
 print(" what are you doing in this here town boy? ")
 pancakes = input()
 if pancakes == "Looking for a place to stay the night":
+    score = + 5
     print("There is a motel down the lane, you better be out of here by tommorrow")
 else:
     print("Well then leave now or were going to have some trouble now boy")
 tacos = input()
 if tacos == "ill get out of your hair now thankyou for all your help":
+    score = + 5
     print("Alright good day")
 else:
     print("what did you say to me boy")
 icecream = input()
 if icecream == "one more question?":
+    score = + 5
     print ("yes?")
 else:
     print ("you better leave now or ima have to apply force boy")
 funnelcake = input()
 if funnelcake == "where is the church":
+    score = + 5
     print ("down by harpers lane, i got to get going now good day")
 else:
     print ("he punches you, then takes you down to the ground and ties your hands with a small rope.")
@@ -28,12 +32,15 @@ else:
 print("You walk down harpers lane where you see the church you see the open door you walk in and sit and start to pray the pastor walks up to you and says why are you here boy")
 sausage = input()
 if sausage == "looking for guidence":
+    score = + 5
     print("there is a saloon down the road")
 else:
     print("there is a saloon down the lane")
 pizza = input()
 if pizza == "thankyou":
-     print("the sheriff bust into the church and drags you to the gallows and hangs you because of your actions and hangs you and your story is over")
+    score = + 5
+    print("the sheriff bust into the church and drags you to the gallows and hangs you because of your actions and hangs you and your story is over")
 else:
     print("the sheriff bust into the church and drags you to the gallows and hangs you because of your actions and hangs you and your story is over")
 
+print(score)
